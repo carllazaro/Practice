ad10a4ac0e3fff5c4d1bb355d4f60d6b8a4aac34
Carl Lazaro <carllazaro12345@gmail.com>
2025-08-15 10:37:57 +0800

    Practice

---
+++ B8.py
@@ -0,0 +1,8 @@
+# Copy Specific Elements From Tuple
+# Write a program to copy elements 44 and 55 from the following tuple into a new tuple.
+# Expected output:
+# tuple2: (44, 55)
+
+tuple1 = (11, 22, 33, 44, 55, 66)
+tuple2 = tuple1[3:-1]
+print(tuple2)
